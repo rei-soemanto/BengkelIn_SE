8a0ffddb993566dc5eabdb41ec4c5e2ff091dd12
Rei Soemanto <reresoemanto@gmail.com>
2026-05-01 18:20:36 +0700

    Initial Commit

---
+++ BengkelIn_SE/BengkelIn_SEApp.swift
@@ -0,0 +1,17 @@
+//
+//  BengkelIn_SEApp.swift
+//  BengkelIn_SE
+//
+//  Created by Rei Soemanto on 01/05/26.
+//
+
+import SwiftUI
+
+@main
+struct BengkelIn_SEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
